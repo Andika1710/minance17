9820d959c19ae97059edc45fdd25d8a6f1441c4a
AlvinSebayang <schristoper22@gmail.com>
2024-08-29 09:24:39 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// start root file
+// do not delete this!!!
+import  './script/custom.js'
+import './style/main.scss';
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+// end root file
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    screens: {
+      'xs': '375px',    // Extra small devices (phones)
+      'sm': '640px',    // Small devices (tablets)
+      'md': '768px',    // Medium devices (small laptops)
+      'lg': '1024px',   // Large devices (desktops)
+      'xl': '1280px',   // Extra large devices (large desktops)
+      '2xl': '1440px',  // 2x Extra large devices (larger screens)
+    },
+    container: {
+      center: true,  // Center container by default
+      padding: {
+        DEFAULT: '1rem', // Padding default
+        sm: '1.5rem',  // Padding for small screens
+        md: '2rem',    // Padding for medium screens
+        lg: '2.5rem',  // Padding for large screens
+        xl: '3rem',    // Padding for extra large screens
+      },
+    },
+  },
+  plugins: [],
+}
+
